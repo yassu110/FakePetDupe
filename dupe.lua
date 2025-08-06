80d6201b50177fe8a291a5f6f768c86b1ee39399
yassu110 <yduupo@gmail.com>
2025-08-06 22:38:40 +0100

    Create dupe.lua

---
+++ dupe.lua
@@ -0,0 +1,25 @@
+-- Fake Pet Dupe Script (Client-Side Only)
+
+local fakePetName = "Super Dog"
+local dupeAmount = 5
+
+local player = game.Players.LocalPlayer
+local backpack = player:WaitForChild("Backpack") or player:FindFirstChild("Backpack")
+local petsFolder = player:FindFirstChild("Pets") or Instance.new("Folder", player)
+petsFolder.Name = "Pets"
+
+local function createFakePet(name)
+    local pet = Instance.new("Folder")
+    pet.Name = name .. "_" .. tostring(math.random(1000, 9999))
+    pet.Parent = petsFolder
+end
+
+for i = 1, dupeAmount do
+    createFakePet(fakePetName)
+end
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "FAKE DUPE";
+    Text = fakePetName.." x"..dupeAmount.." added (client-side)";
+    Duration = 5;
+})
